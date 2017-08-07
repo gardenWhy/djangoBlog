0bd3bbbcee96e7efea3bf318816ef54fb178faf8
gardenWhy <1057013169@qq.com>
2017-08-07 11:26:55 +0800

    完成数据库的简单设置
    完成数据库的简单设置

--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -17,15 +17,20 @@ class Category(models.Model):#分类
     https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
     """
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):#标签
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
     
 class Post(models.Model):
     #文章涉及字段多，需要创建多个列
     
     #文章标题
-    titlt = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     #文章正文，用的是TextField类型，因为正文将会是一大段
     body = models.TextField()
     #创建时间和最后一次修改时间，用的是DateTimeField类型。
@@ -51,4 +56,7 @@ class Post(models.Model):
     一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系
     """
     author = models.ForeignKey(User)
+    def __str__(self):
+        return self.name
+
 # Create your models here.
