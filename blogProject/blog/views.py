97e2159b6e76ac2785c105979199392e4e369c7c
gardenWhy <1057013169@qq.com>
2017-08-07 14:00:48 +0800

    完成简单的视图显示
    完成views的编辑，完成简单的视图显示

--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def index(request):
+    return render(request,'blog/index.html',context={
+                        'title': '我的博客首页',
+                        'welcome': '欢迎访问我的博客首页'
+            })
 # Create your views here.
