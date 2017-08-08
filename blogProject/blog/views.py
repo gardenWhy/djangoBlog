4a724757d2ff6fa0cfc37843bb74d547679c0833
gardenWhy <1057013169@qq.com>
2017-08-08 17:15:54 +0800

    完成主页面模板
    添加了主页模板

--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request,'blog/index.html',context={
-                        'title': '我的博客首页',
-                        'welcome': '欢迎访问我的博客首页'
+                        'post_list': post_list
             })
 # Create your views here.
